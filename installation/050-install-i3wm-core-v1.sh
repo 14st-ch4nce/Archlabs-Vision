64c0c3efe0fe8174655d0f4e32b23d2275439ebf
Erik Dubois <erik.dubois@gmail.com>
2017-06-20 20:15:59 +0200

    Comment : update on Tue Jun 20 20:15:59 CEST 2017

--- installation/050-install-i3wm-core-v1.sh
+++ installation/050-install-i3wm-core-v1.sh
@@ -23,50 +23,6 @@ sudo pacman -S --noconfirm --needed i3lock i3status
 
 
 
-# gnome
-
-echo "################################################################"
-echo "j4-dmenu-desktop"   
-echo "################################################################"
-
-package="j4-dmenu-desktop"
-
-#----------------------------------------------------------------------------------
-
-#checking if application is already installed or else install with aur helpers
-if pacman -Qi $package &> /dev/null; then
-
-	echo "################################################################"
-	echo "################## "$package" is already installed"
-	echo "################################################################"
-
-else
-
-	#checking which helper is installed
-	if pacman -Qi packer &> /dev/null; then
-
-		echo "Installing with packer"
-		packer -S --noconfirm --noedit  $package
-
-	elif pacman -Qi pacaur &> /dev/null; then
-		
-		echo "Installing with pacaur"
-		pacaur -S --noconfirm --noedit  $package
-		 	
-	elif pacman -Qi yaourt &> /dev/null; then
-
-		echo "Installing with yaourt"
-		yaourt -S --noconfirm $package
-			  	
-	fi
-
-
-fi
-
-
-
-
-
 
 echo "################################################################"
 echo "i3blocks"   
